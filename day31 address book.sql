d8ceb95e480b0aabd155691718aa695e20539ea9
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-02-22 00:01:59 +0530

    insertion of person details in table

---
+++ day31 address book.sql
@@ -0,0 +1,29 @@
+--UC1 Creating of Database
+create database Address_book_serviceDB 
+select * from sys.databases
+
+--UC2 creating of table with firstname,last name,address,city,zipcode,phonenumber & email \
+
+create table Addressbook
+(
+Firstname varchar(60),
+Lastname varchar(50),
+Address varchar(150),
+City varchar(50),
+Zipcode varchar(6),
+Phonenumber varchar(13),
+Email varchar(60)
+)
+
+select * from Addressbook
+
+--UC3 inserting of new contacts
+
+insert into Addressbook values
+('Arun','kumar','Telangana','peddapalli','505208','1596321478','arun@gmail.com'),
+('Naven','mabbu','Telangana','Hyderabad','500063','8697412456','naveen@gmail.com'),
+('Neha','patel','Bangalore','Karnataa','503698','4563217895','neha@gmail.com'),
+('Arnavi','singh','Chennai','Tamilnadu','569238','7531598625','arnavi@gmail.com'),
+('Swathi','roy','cochin','kerala','456218','785123654','swathi@gmail.com')
+
+select * from Addressbook
